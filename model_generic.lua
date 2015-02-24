d9f23922983fa3c401e6de49d4bb45049dcadcb8
Mehdi Cherti <mehdi@cherti.name>
2015-02-24 07:23:42 +0100

    update

--- model_generic.lua
+++ model_generic.lua
@@ -44,11 +44,11 @@ train_obj = helpers.get_data_and_column_names(opt.train)
 test = csv2tensor.load(opt.test)
 
 targets, proba = model(train_obj.X, train_obj.y, test)
-
+print(targets:size())
+print(proba:size())
 targets = torch.Tensor.totable(targets)
 proba = torch.Tensor.totable(proba)
-print(targets:dim())
-print(proba:dim())
+
 for i, k in ipairs(targets) do
     targets[i] = {tostring(targets[i])}
 end
